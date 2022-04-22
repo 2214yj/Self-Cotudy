98606b71b7aca7aa2d6a0bd040e8ef65878da342
kim-yeji <2214yjgm@gmail.com>
2022-04-22 23:57:14 +0900

    고급 자바 실습

---
+++ 고급자바프로그래밍/src/7week/src/test/Dog.java
@@ -0,0 +1,25 @@
+package test;
+
+public class Dog {
+    private String name;
+    private char gender;
+    private int age;
+
+    public Dog(String name, char gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
