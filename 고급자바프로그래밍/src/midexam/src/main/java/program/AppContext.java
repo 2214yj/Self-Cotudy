214487e7fd5e0d3266466b1e99769bf4d792c094
kim-yeji <2214yjgm@gmail.com>
2022-04-25 05:07:29 +0900

    고급 자바 실습

---
+++ 고급자바프로그래밍/src/midexam/src/main/java/program/AppContext.java
@@ -0,0 +1,23 @@
+package program;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "program")
+public class AppContext {
+
+    @Bean
+    @Qualifier("printer")
+    public MemberPrinter memberPrinter1() {
+        return new MemberPrinter();
+    }
+
+    @Bean
+    @Qualifier("summaryPrinter")
+    public MemberSummaryPrinter memberPrinter2() {
+        return new MemberSummaryPrinter();
+    }
+}
